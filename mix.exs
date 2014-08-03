91b63b9e85c62d46c8147e2d4a5e25bb36f704d2
BennyHallett <benny.hallett@gmail.com>
2014-08-03 23:00:17 +1000

    Updated mix file for packaging

--- mix.exs
+++ mix.exs
@@ -3,28 +3,28 @@ defmodule RSS.Mixfile do
 
   def project do
     [app: :rss,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 0.14.3",
+     package: package,
+     docs: [readme: true, main: "README.md"],
+     description: """
+     A super simple RSS feed builder
+     """,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: []]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{ "Github" => "https://github.com/bennyhallett/elixir-rss" }
+    }
   end
 end
