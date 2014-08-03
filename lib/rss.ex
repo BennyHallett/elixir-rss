2828ce4c730211e59a94355fd28f8d796c915c22
BennyHallett <benny.hallett@gmail.com>
2014-08-03 22:39:07 +1000

    Build complete feed

--- lib/rss.ex
+++ lib/rss.ex
@@ -1,5 +1,17 @@
 defmodule RSS do
 
+  def feed(channel, items) do
+    content = """
+    <?xml version="1.0" encoding="utf-8"?>
+    <rss version="2.0">
+    <channel>
+    #{channel}#{Enum.join items, ""}</channel>
+    </rss>
+    """
+    
+    
+  end
+
   def item(title, desc, pubDate, link, guid) do
     """
     <item>
@@ -14,13 +26,11 @@ defmodule RSS do
 
   def channel(title, link, desc, date, lang) do
     """
-    <channel>
       <title>#{title}</title>
       <link>#{link}</link>
       <description>#{desc}</description>
       <lastBuildDate>#{date}</lastBuildDate>
       <language>#{lang}</language>
-    </channel>
     """
   end
 
--- test/rss_test.exs
+++ test/rss_test.exs
@@ -27,13 +27,44 @@ defmodule RSSTest do
     lang = "en-us"
 
     assert RSS.channel(title, link, desc, date, lang) == """
-    <channel>
       <title>#{title}</title>
       <link>#{link}</link>
       <description>#{desc}</description>
       <lastBuildDate>#{date}</lastBuildDate>
       <language>#{lang}</language>
+    """
+  end
+
+  test "build feed" do
+    channel = RSS.channel("Test blog", "http://test.blog", "This is a test blog", "Mon, 12 Sep 2005 18:37:00 GMT", "en-us")
+    item2 = RSS.item("Post 2", "The second post", "Mon, 12 Sep 2005 18:37:00 GMT", "http://test.blog/two", "http://test.blog/two")
+    item1 = RSS.item("Post 1", "The first post", "Sun, 11 Sep 2005 18:37:00 GMT", "http://test.blog/one", "http://test.blog/one")
+
+    assert RSS.feed(channel, [item2, item1]) == """
+    <?xml version="1.0" encoding="utf-8"?>
+    <rss version="2.0">
+    <channel>
+      <title>Test blog</title>
+      <link>http://test.blog</link>
+      <description>This is a test blog</description>
+      <lastBuildDate>Mon, 12 Sep 2005 18:37:00 GMT</lastBuildDate>
+      <language>en-us</language>
+    <item>
+      <title>Post 2</title>
+      <description>[CDATA[ The second post ]]</description>
+      <pubDate>Mon, 12 Sep 2005 18:37:00 GMT</pubDate>
+      <link>http://test.blog/two</link>
+      <guid>http://test.blog/two</guid>
+    </item>
+    <item>
+      <title>Post 1</title>
+      <description>[CDATA[ The first post ]]</description>
+      <pubDate>Sun, 11 Sep 2005 18:37:00 GMT</pubDate>
+      <link>http://test.blog/one</link>
+      <guid>http://test.blog/one</guid>
+    </item>
     </channel>
+    </rss>
     """
   end
 end
