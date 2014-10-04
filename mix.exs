3407b3250de04eb59bc8d156d8ab06e17bf4e852
BennyHallett <benny.hallett@gmail.com>
2014-10-04 10:56:36 +1000

    Moved RSS to Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule RSS.Mixfile do
 
   def project do
     [app: :rss,
-     version: "0.2.0",
-     elixir: "~> 0.14",
+     version: "0.2.1",
+     elixir: "~> 1.0.0",
      package: package,
      docs: [readme: true, main: "README.md"],
      description: """
