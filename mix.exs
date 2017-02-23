98eed327e5dadc40ae9944c670d13e865032f7f2
Benny Hallett <benny.hallett@gmail.com>
2017-02-23 23:17:10 +1100

    Fixed compile warnings since Elixir version upgrade

--- mix.exs
+++ mix.exs
@@ -5,21 +5,19 @@ defmodule RSS.Mixfile do
     [app: :rss,
      version: "0.2.1",
      elixir: "~> 1.4",
-     package: package,
+     package: package(),
      docs: [readme: true, main: "README.md"],
      description: """
      A super simple RSS feed builder
      """,
-     deps: deps]
+     deps: deps()]
   end
 
   def application do
     [applications: []]
   end
 
-  defp deps do
-    []
-  end
+  defp deps, do: []
 
   defp package do
     %{
